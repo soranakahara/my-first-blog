75f9a47738fce91db7e0db2eb721c4e493a76e11
Sora <skyfish1003@gmail.com>
2018-10-19 09:57:38 +0900

    My django book app

---
+++ mybook/wsgi.py
@@ -0,0 +1,20 @@
+"""
+WSGI config for mybook project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mybook.settings')
+
+application = get_wsgi_application()
+
+# heroku用設定
+from whitenoise.django import DjangoWhiteNoise
+application = DjangoWhiteNoise(application)
